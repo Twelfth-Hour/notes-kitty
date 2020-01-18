0cc6600200586fb38e4599fc2705fa6912b90ccf
Sahil <sahilbondre@gmail.com>
2020-01-18 12:09:21 +0530

    add test sites to test on

---
+++ test/test.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-labels */
+{
+  clean: {
+    sites: [
+      "https://www.javatpoint.com/breadth-first-search-algorithm",
+      "https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/",
+      "https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/tutorial/",
+      "https://en.wikipedia.org/wiki/Breadth-first_search",
+      "https://www.tutorialspoint.com/data_structures_algorithms/breadth_first_traversal.htm",
+      "https://medium.com/basecs/breaking-down-breadth-first-search-cebe696709d9",
+      "https://www.w3schools.in/data-structures-tutorial/intro/"
+    ];
+  }
+}
