ab161a83153c7b9cb232ba636a36bc525c21a47c
AemieJ <aemie.j@hotmail.co.uk>
2020-01-18 15:22:23 +0530

    add functionality for define query

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,22 @@
-function handleDefine() {
-  alert("Defining");
+async function handleDefine() {
+  let query = document.getElementById("input-box").value;
+  const url = `https://api.duckduckgo.com/?q=${query}&format=json&pretty=1`;
+  let response = await fetch(url);
+  /* eslint-disable-next-line no-unused-vars */
+  let data = await response.json();
+  // alert(data.Abstract);
 }
 
 function handleSearch() {
-  alert("Searching");
+  let query = document.getElementById("input-box").value;
+  const url = `https://duckduckgo.com/?q=${query}`;
+  /* eslint-disable-next-line no-undef*/
+  chrome.tabs.create({
+    url: url
+  });
 }
 
-document.getElementById("define-button").addEventListener("click", () => {
+document.getElementById("define-button").addEventListener("click", async () => {
   handleDefine();
 });
 
