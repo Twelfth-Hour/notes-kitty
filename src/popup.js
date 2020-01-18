7d642d54c2674a0e55955e428214131e30fc509a
Sahil <sahilbondre@gmail.com>
2020-01-18 15:39:18 +0530

    add define feature

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,18 @@
 async function handleDefine() {
   let query = document.getElementById("input-box").value;
+  if (query === "") {
+    document.getElementById("definition").innerHTML =
+      "<code>Enter Search Query</code>";
+    return;
+  }
   const url = `https://api.duckduckgo.com/?q=${query}&format=json&pretty=1`;
   let response = await fetch(url);
   /* eslint-disable-next-line no-unused-vars */
-  let data = await response.json();
-  // alert(data.Abstract);
+  let { Abstract } = await response.json();
+  if (Abstract === "") {
+    Abstract = "<code>cannot find definition, try searching</code>";
+  }
+  document.getElementById("definition").innerHTML = Abstract;
 }
 
 function handleSearch() {
