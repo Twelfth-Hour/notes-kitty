51a064b862880fcf5bdb405cd1077e6a3ede379e
AemieJ <aemie.j@hotmail.co.uk>
2020-01-18 12:45:00 +0530

    add communication between background and content

--- background.js
+++ background.js
@@ -1,2 +1,11 @@
 /* eslint-disable no-undef*/
-chrome.runtime.onInstalled.addListener(function() {});
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.runtime.onMessage.addListener(function(message, _, sendResponse) {
+    if(message == "Run background") {
+      chrome.tabs.onUpdated.addListener(function() {
+
+      });
+      sendResponse("Query completed");
+    }
+  });
+});
---
+++ content.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef*/
+let currentLocation = window.location.href;
+
+if ( currentLocation.includes("duckduckgo")) {
+  chrome.runtime.sendMessage({
+    "message": "Run background"
+  }, function(response) {
+    console.log(response);
+  });
+}
